9826ac5e54c3d74d76e3641b6a6aa25a2948ccdf
Anjelica <ambrosio.anjelica@gmail.com>
2020-11-13 12:51:05 -0800

    users can search name and breed

--- test/test-pets.js
+++ test/test-pets.js
@@ -18,11 +18,11 @@ chai.use(chaiHttp);
 
 describe('Pets', ()  => {
 
-  after(() => { 
+  after(() => {
     Pet.deleteMany({$or: [{name: 'Norman'}, {name: 'Spider'}] }).exec((err, pets) => {
       console.log(pets)
       pets.remove();
-    }) 
+    })
   });
 
   // TEST INDEX
@@ -46,8 +46,8 @@ describe('Pets', ()  => {
           done();
         });
   });
-  
-  // TEST CREATE 
+
+  // TEST CREATE
   it('should create a SINGLE pet on /pets POST', (done) => {
     chai.request(server)
         .post('/pets')
@@ -103,6 +103,17 @@ describe('Pets', ()  => {
       });
     });
   });
+
+  // SEARCH
+it('should search ALL pets by name on /search GET', (done) => {
+  chai.request(server)
+      .get('/search?term=norman')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+});
 
   // TEST DELETE
   it('should delete a SINGLE pet on /pets/<id> DELETE', (done) => {
