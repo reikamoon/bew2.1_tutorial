51b751f8fec2af1545931b56a25b8ed4f592f15c
Anjelica <ambrosio.anjelica@gmail.com>
2020-11-13 12:49:15 -0800

    updated search to include breed

--- routes/pets.js
+++ routes/pets.js
@@ -50,13 +50,16 @@ module.exports = (app) => {
   });
 
   // SEARCH PET
-app.get('/search', (req, res) => {
-  term = new RegExp(req.query.term, 'i')
+  app.get('/search', (req, res) => {
+    term = new RegExp(req.query.term, 'i')
 
-  Pet.find({'name': term}).exec((err, pets) => {
-    res.render('pets-index', { pets: pets });
-  })
-});
+    Pet.find({$or:[
+      {'name': term},
+      {'species': term}
+    ]}).exec((err, pets) => {
+      res.render('pets-index', { pets: pets });
+    })
+  });
 
   // DELETE PET
   app.delete('/pets/:id', (req, res) => {
