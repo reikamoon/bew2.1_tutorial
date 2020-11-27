6548da4669f13b087c75ee4cf431af400a89a08e
Anjelica <ambrosio.anjelica@gmail.com>
2020-11-27 15:27:20 -0800

    added AWS client const

--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,34 @@
 // MODELS
 const Pet = require('../models/pet');
 
+// UPLOADING TO AWS S3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AKIAIZCRPIEQW2NA6DJQ,
+    secretAccessKey: process.env.WNpK9xJ1COSjG9IglPuuHisZeFWO7HxVEki3xLyq
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // PET ROUTES
 module.exports = (app) => {
 
